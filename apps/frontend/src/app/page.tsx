d88db9c84f4b586f6a19eef60564724c94ae3db8
AnonymousFreelancer17 <AnonymousFreelancer17@gmail.com>
2025-05-15 15:26:00 +0530

    initalCommit

---
+++ apps/frontend/src/app/page.tsx
@@ -0,0 +1,13 @@
+
+
+
+export default function Index() {
+ 
+  return (
+     <div className="w-screen h-screen flex justify-center items-center">
+       <h1 className="text-3xl">
+        Hello
+       </h1>
+     </div>
+  );
+};
